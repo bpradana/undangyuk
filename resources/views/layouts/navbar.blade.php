a2c7d07e93c93f6b3f5844e8ab18c0e0f43d1517
nurhabibrs <nurhabibrizkisaputro@gmail.com>
2021-11-29 23:52:06 +0700

    edit UI design in main

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,29 +1,39 @@
 <nav>
-    <ul>
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/designs">Designs</a>
-        </li>
-        @auth
-            <li>
-                <a href='/transactions'>Transactions</a>
-            </li>
-            <li>
-                <a href='/messages'>Messages</a>
-            </li>
-            <li>
-                <a href='/profile'>{{ auth()->user()->name }}</a>
-            </li>
-            <form action="/logout" method="post">
-                @csrf
-                <button type="submit">Logout</button>
-            </form>
-        @else
-            <li>
-                <a href="/login">Login</a>
-            </li>
-        @endauth
-    </ul>
+    <a href="/" class="brand-logo"><b>UndangYuk!</b></a>
+    <div class="container">
+        
+        <div class="nav-wrapper">
+            
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/designs">Designs</a>
+                </li>
+                @auth
+                    <li>
+                        <a href='/transactions'>Transactions</a>
+                    </li>
+                    <li>
+                        <a href='/messages'>Messages</a>
+                    </li>
+                    <li id="nav-mobile" class="right hide-on-med-and-down">
+                        <a href='/profile'>{{ auth()->user()->name }}</a>
+                    </li>
+                    <li id="nav-mobile" class="right hide-on-med-and-down">
+                        <form action="/logout" method="post">
+                            @csrf
+                            <button type="submit">Logout</button>
+                        </form>
+                    </li>
+                @else
+                    <li id="nav-mobile" class="right hide-on-med-and-down">
+                        <a href="/login" class="waves-effect waves-light btn blue">Log in</a>
+                    </li>
+                @endauth
+            </ul>
+            
+        </div>
+    </div>
 </nav>
