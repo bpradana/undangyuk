dbc8c925e2c4201c86caea6f03d55d61b5a5ada3
Bintang Pradana Erlangga Putra <bintangpradana02@student.uns.ac.id>
2021-12-24 20:42:58 +0700

    fix some buttons, title, and layout on design page

--- resources/views/designs/show.blade.php
+++ resources/views/designs/show.blade.php
@@ -1,55 +1,63 @@
 @extends('layouts.main', ['title' => $title])
 
 @section('content')
-<img src="{{asset('assets/img/gambar1depan.jpg')}}" alt=""/>
-<h1>{{ $design->title }}</h1>
-<h2>Price: {{ $design->price }}</h2>
-<h2>Rating: {{ $design->rating }}</h2>
-<p>{{ $design->description }}</p>
-<p>By: {{ $design->user->name }}</p>
-<p>Created at: {{ $design->created_at }}</p>
+<div class="row">
+    <div class="col s6">
+        <img class="responsive-img" src="{{asset('assets/img/gambar1depan.jpg')}}" alt=""/>
+    </div>
+    <div class="col s6">
+        <h1>{{ $design->title }}</h1>
+        <h2>Price: {{ $design->price }}</h2>
+        <h2>Rating: {{ $design->rating }}</h2>
+        <p>{{ $design->description }}</p>
+        <p>By: {{ $design->user->name }}</p>
+        <p>Created at: {{ $design->created_at }}</p>
 
-@auth
-@if ($design->user->id == auth()->user()->id)
-    <a href="/edit-design/{{ $design->id }}">Edit</a>
-    <form action="/delete-design/{{ $design->id }}" method="post">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Delete</button>
-    </form>
-@else
-    <a href="/create-transaction/{{ $design->id }}">Buy</a>
-@endif
-@endauth
+        @auth
+            @if ($design->user->id == auth()->user()->id)
+                <a class="btn pink accent-4" href="/edit-design/{{ $design->id }}">Edit</a>
+                <form action="/delete-design/{{ $design->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn pink accent-4" type="submit">Delete</button>
+                </form>
+            @else
+                <a class="btn pink accent-4" href="/create-transaction/{{ $design->id }}">Buy</a>
+            @endif
+        @else
+            <a class="btn pink accent-4" href="/login">Buy</a>
+        @endauth
 
-<h2>Comments:</h2>
+        <h2>Comments:</h2>
 
-@auth
-<form action="/create-comment/{{ $design->id }}" method="post">
-    @csrf
-    <input type="text" id="title" name="title" placeholder="Title"><br>
-    <textarea id="body" name="body" placeholder="Comment"></textarea><br>
-    <input type="number" id="rating" name="rating" placeholder="Rating" min=0 max=5><br>
-    <button type="submit">Submit</button>
-</form>
-@endauth
-
-@foreach ($design->comments as $comment)
-    <h3>{{ $comment->title }}</h3>
-    <h4>Rating: {{ $comment->rating }}</h4>
-    <p>{{ $comment->body }}</p>
-    <p>By: {{ $comment->user->name }}</p>
-    <p>Created at: {{ $comment->created_at }}</p>
-    @auth
-    @if ($comment->user_id == auth()->user()->id)
-        <form action="/delete-comment/{{ $comment->id }}" method="post">
+        @auth
+        <form action="/create-comment/{{ $design->id }}" method="post">
             @csrf
-            @method('DELETE')
-            <button type="submit">Delete</button>
+            <input type="text" id="title" name="title" placeholder="Title"><br>
+            <textarea id="body" name="body" placeholder="Comment"></textarea><br>
+            <input type="number" id="rating" name="rating" placeholder="Rating" min=0 max=5><br>
+            <button class="btn pink accent-4" type="submit">Submit</button>
         </form>
-    @endif
-    @endauth
-    <hr>
-@endforeach
+        @endauth
+
+        @foreach ($design->comments as $comment)
+            <h3>{{ $comment->title }}</h3>
+            <h4>Rating: {{ $comment->rating }}</h4>
+            <p>{{ $comment->body }}</p>
+            <p>By: {{ $comment->user->name }}</p>
+            <p>Created at: {{ $comment->created_at }}</p>
+            @auth
+            @if ($comment->user_id == auth()->user()->id)
+                <form action="/delete-comment/{{ $comment->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn pink accent-4" type="submit">Delete</button>
+                </form>
+            @endif
+            @endauth
+            <hr>
+        @endforeach
+    </div>
+</div>
 
 @endsection
